f45fcfa1120db24673cb7cf630ccb0f12aef8fb5
Oscar Andrés Díaz <info@oscarandresdiaz.com>
2017-01-03 21:54:10 -0500

    Add make_virtualenv_on_mac.bash file
    To configure a virtual environment on MacOS

---
+++ make_virtualenv_on_mac.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Mac OS X platform"
+  if [ ! -d ./venv/ ]; then
+    pyvenv-3.5 venv
+    source venv/bin/activate
+    pip install --upgrade pip
+    #brew install postgres
+    export PATH=$PATH:/usr/local/Cellar/postgresql/9.6.1/bin/
+    export LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib"
+    pip install -r requirements.txt
+    #chmod u+x manage.py
+    deactivate
+  else
+    source venv/bin/activate
+    pip install -r requirements.txt
+    #python manage.py collectstatic
+    deactivate
+  fi
+else
+  echo "Not Mac OS X platform"
+fi
